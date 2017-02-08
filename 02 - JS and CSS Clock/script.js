b08c4ffce740ca1c1b5e4929709298fcac762e89
bryan buitrago <bryanbuitrago86@gmail.com>
2017-02-07 23:56:36 -0500

    clock finished

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; //offsets the default 90 degrees
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  // console.log(seconds);
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360 ) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  // console.log(minutes);
+
+  const hours = now.getMinutes();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  // console.log(hours);
+
+
+  // console.log(minutes);
+}
+
+setInterval(setDate, 1000);
